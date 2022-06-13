0315b33b2929e67b49f8c9df07d8903206e70075
BurnsD <Dustinburnsactech@gmail.com>
2022-06-13 13:31:27 -0500

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+
 
 function App() {
   return (
+    <>
+    <Nav />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+    </>
   );
 }
 
